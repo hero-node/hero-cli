7ae2995b3f6f278f17074cc3980016710e30a322
benlv <benlv.hu@dianrong.com>
2017-04-06 14:49:09 +0800

    Usage Show

--- bin/index.js
+++ bin/index.js
@@ -5,10 +5,33 @@
 var spawn = require('cross-spawn');
 var script = process.argv[2];
 var args = process.argv.slice(3);
-var pkg = require('../package.json');
-// var yargs = require('yargs');
+var yargs = require('yargs');
+var fs = require('fs');
 var result = null;
 
+function showUsage() {
+    var argv = yargs
+      .usage('Usage: $0 <command> [options]')
+      .command(['init [project-directory]'], 'Initialize workspace, copy project and other basic configuration')
+      .example('$0 init my-hero-app')
+      .demandOption(['init'])
+      .epilog('copyright 2017')
+      .argv;
+
+    var s = fs.createReadStream(argv.file);
+
+    var lines = 0;
+
+    s.on('data', function (buf) {
+        lines += buf.toString().match(/\n/g).length;
+    });
+
+    s.on('end', function () {
+        console.log(lines);
+    });
+
+    console.log('See: https://github.com/dianrong/Hero');
+}
 switch (script) {
     case 'build':
     case 'init':
@@ -31,9 +54,6 @@ switch (script) {
         process.exit(result.status);
         break;
     default:
-        console.log('Unknown script "' + script + '".');
-        console.log('Perhaps you need to update ' + pkg.name + '?');
-        console.log('See: https://github.com/dianrong/Hero');
-        // Todo: Console Log Usages
+        showUsage();
         break;
 }
