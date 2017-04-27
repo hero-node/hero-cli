e69538875c4d458cf0107ffc118a21e13dd181b5
benlv <benlv.hu@dianrong.com>
2017-04-27 14:48:12 +0800

    Remove Babel Config for Mock Server

--- scripts/init.js
+++ scripts/init.js
@@ -44,7 +44,7 @@ function generate(appName) {
 
     shell.cp('-Rf', templatePath + '/*', targetPath);
 
-    var dotFiles = ['babelrc', 'editorconfig', 'gitattributes', 'gitignore', 'hero-cli.json'];
+    var dotFiles = ['editorconfig', 'gitattributes', 'gitignore', 'hero-cli.json'];
 
     dotFiles.forEach(file => {
         shell.mv('-f', path.join(targetPath, file), path.join(targetPath, '.' + file));
