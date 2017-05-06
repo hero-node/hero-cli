14831d36458d427c926522fe4f261414b75c36c9
benlv <benlv.hu@dianrong.com>
2017-05-06 14:49:44 +0800

    Update Template Code

--- template/src/index.js
+++ template/src/index.js
@@ -1,22 +1,36 @@
-import 'hero-js/src/hero-app.html';
+function init() {
+  // Hero Core Web Components
+    require('hero-js/src/hero-app.html');
+    var args = {};
+    var params = (window.location.search.split('?')[1] || '').split('&');
+    var paramParts;
+    var param;
 
-var args = {};
-var params = (window.location.search.split('?')[1] || '').split('&');
-var paramParts;
-
-for (var param in params) {
-    if (params.hasOwnProperty(param)) {
-        paramParts = params[param].split('=');
-        args[paramParts[0]] = decodeURIComponent(paramParts[1] || '');
+    for (param in params) {
+        if (params.hasOwnProperty(param)) {
+            paramParts = params[param].split('=');
+            args[paramParts[0]] = decodeURIComponent(paramParts[1] || '');
+        }
     }
+    var app = { tabs: [
+    { url: args.state, title: '首页', class: 'DRViewController', image: 'home_green' }
+    ] };
+
+    window.document.write('<hero-app json=' + JSON.stringify(app) + '></hero-app>');
 }
-var app = { tabs: [
-  { url: args.state, title: '首页', class: 'DRViewController', image: 'home_green' }
-] };
 
-window.document.write('<hero-app json=' + JSON.stringify(app) + '></hero-app>');
+var supportWebComponent = ('registerElement' in document
+    && 'import' in document.createElement('link')
+    && 'content' in document.createElement('template'));
 
-var arr = [2, 3];
+if (!supportWebComponent) {
+    require.ensure(['../public/lib/webcomponents-lite'], function () {
+        window.Polymer = { lazyRegister: true, dom: 'shadow' };
+        require('../public/lib/webcomponents-lite');
+        init();
+    });
+} else {
+    init();
+}
 
-console.log(...arr);
-console.log(process.env);
+console.log('process.env', process.env);
