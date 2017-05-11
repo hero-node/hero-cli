72d555af8bea60a96c278e864d2b796f51d43201
benlv <benlv.hu@dianrong.com>
2017-05-11 19:29:52 +0800

    Revert Insert to Body && Update Template

--- template/src/index.js
+++ template/src/index.js
@@ -1,47 +1,35 @@
 import { PATH as path } from './constant/index';
-
 function init() {
-    // Hero Core Web Components
     require('hero-js/src/hero-app.html');
     var args = {};
     var params = (window.location.search.split('?')[1] || '').split('&');
     var paramParts;
-    var param;
 
-    for (param in params) {
+    for (var param in params) {
         if (params.hasOwnProperty(param)) {
             paramParts = params[param].split('=');
             args[paramParts[0]] = decodeURIComponent(paramParts[1] || '');
         }
     }
-
     var app = { tabs: [
-      { url: args.state || path + '/pages/start.html', title: '首页' }
+    { url: args.state || path + '/pages/start.html', title: '首页' }
     ] };
-
-    var appHero = document.createElement('hero-app');
+    var app1 = document.createElement('hero-app');
 
     if (!window.document.body) {
         document.body = document.createElement('body');
     }
-    window.document.body.appendChild(appHero);
-    appHero.json = JSON.stringify(app);
-
-    window.document.write('<hero-app json=' + JSON.stringify(app) + '></hero-app>');
+    window.document.body.appendChild(app1);
+    app1.json = JSON.stringify(app);
 }
 
-// Dynamic Load WebComponents Polyfills
-var supportWebComponent = (
-  'registerElement' in document
-  && 'import' in document.createElement('link')
-  && 'content' in document.createElement('template'));
+var supportWebComponent = ('registerElement' in document
+    && 'import' in document.createElement('link')
+    && 'content' in document.createElement('template'));
 
 if (!supportWebComponent) {
     require.ensure(['../public/lib/webcomponents-lite'], function () {
-        window.Polymer = {
-            lazyRegister: true,
-            dom: 'shadow'
-        };
+        window.Polymer = { lazyRegister: true, dom: 'shadow' };
         require('../public/lib/webcomponents-lite');
         init();
     });
@@ -49,5 +37,4 @@ if (!supportWebComponent) {
     init();
 }
 
-// You can access all the global variables via `process.env`
 console.log('process.env', process.env);
