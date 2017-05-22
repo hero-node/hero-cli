7e7ba3363e075cea94e88891c2e6ad339cdfc06a
benlv <benlv.hu@dianrong.com>
2017-05-22 19:18:19 +0800

    Add 'hero platform build android' command to Android Build APK

---
+++ scripts/platform.js
@@ -0,0 +1,95 @@
+'use strict';
+
+var fs = require('fs');
+var path = require('path');
+var chalk = require('chalk');
+var yargs = require('yargs');
+var shell = require('shelljs');
+var pgk = require('../package.json');
+var commandName = Object.keys(pgk.bin)[0];
+var paths = require('../config/paths');
+var getBuildToosVersion = require('../lib/getAndroidVersions');
+var checkRequiredFiles = require('../lib/checkRequiredFiles');
+
+var ANDROID = 'android';
+var IOS = 'ios';
+
+global.argv = yargs.argv;
+
+function showUsage() {
+
+    console.log();
+    console.log('Please specify the platform: android or ios.');
+    console.log(chalk.cyan('    ' + commandName + ' platform build ') + chalk.green('<name>'));
+    console.log();
+    console.log('For example:');
+    console.log(chalk.cyan('    ' + commandName + ' platform build android'));
+    console.log();
+
+    process.exit(1);
+}
+if (global.argv.h ||
+    global.argv._[0] !== 'build') {
+    showUsage();
+}
+if (global.argv._[1] !== IOS && global.argv._[1] !== ANDROID) {
+    showUsage();
+}
+// Warn and crash if required files are missing
+if (!checkRequiredFiles([paths.heroCliConfig])) {
+    console.log();
+    console.log('Please make sure this is a hero application.');
+    console.log();
+    process.exit(1);
+}
+
+var appType = global.argv._[1];
+var isWindows = '';
+var encoding = 'UTF-8';
+
+function handleAndroidFiles() {
+    var buildVersion = getBuildToosVersion();
+
+    var gradleTemplate = path.join(__dirname, '../config/build.gradle');
+    var content = fs.readFileSync(gradleTemplate, {
+        encoding: encoding
+    });
+
+    content = content.replace('__compileSdkVersion', buildVersion.compileSdkVersion)
+      .replace('__buildToolsVersion', buildVersion.buildToolsVersion)
+      .replace('__supportAnnotationVersion', buildVersion.supportLibVersion);
+
+    var gradleFile = path.join(paths.appSrc, '../platforms/', appType, '/app/build.gradle');
+
+    fs.writeFileSync(gradleFile, content, {
+        encoding: encoding
+    });
+}
+function handleIOSFiles() {
+}
+function generateApp() {
+    if (appType === IOS) {
+        handleIOSFiles();
+    } else {
+        handleAndroidFiles();
+    }
+
+    var command = './gradlew';
+
+    if (isWindows) {
+        command += '.bat';
+    }
+    command += ' assembleDebug';
+
+    console.log(path.join(paths.appSrc, '../platforms/', appType));
+
+    shell.exec(command, {
+        cwd: path.join(paths.appSrc, '../platforms/', appType),
+        env: {
+            ANDROID_HOME: process.env.ANDROID_HOME
+        }
+    });
+
+}
+
+generateApp();
