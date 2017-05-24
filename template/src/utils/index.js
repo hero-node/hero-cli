61b8e11abd1358b009836bf1cf69607302e153a4
benlv <benlv.hu@dianrong.com>
2017-05-24 15:01:15 +0800

    Using English Label in Sample Code

--- template/src/utils/index.js
+++ template/src/utils/index.js
@@ -64,13 +64,13 @@ function fmoney(s, n) {
 
 function dataFormat(date, fmt) {
     var o = {
-        'M+': date.getMonth() + 1, // 月份
-        'd+': date.getDate(), // 日
-        'h+': date.getHours(), // 小时
-        'm+': date.getMinutes(), // 分
-        's+': date.getSeconds(), // 秒
-        'q+': Math.floor((date.getMonth() + 3) / 3), // 季度
-        'S': date.getMilliseconds() // 毫秒
+        'M+': date.getMonth() + 1, // Month
+        'd+': date.getDate(), // Day
+        'h+': date.getHours(), // Hour
+        'm+': date.getMinutes(), // Minutes
+        's+': date.getSeconds(), // Second
+        'q+': Math.floor((date.getMonth() + 3) / 3), // Q
+        'S': date.getMilliseconds() // Mill
     };
     var k;
 
